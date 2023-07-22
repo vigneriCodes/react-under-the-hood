9c5e372aeb592d8bed92b2a3ddf73deba9dabbfd
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-22 12:05:29 -0700

    rf into separate component files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 // import ReactDOM from 'react-dom';
 
-import App from "./App";
-import "./index.css";
+import App from './components/App';
+import './index.css';
 
 // REACT 18
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // REACT 17
